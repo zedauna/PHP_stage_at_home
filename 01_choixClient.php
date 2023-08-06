6026fc491b811fe9365bd2ecadabf28a037b9042
Jéros VIGAN <zedauna@programmer.net>
2023-08-06 08:55:25 +0200

    01_choixClient

---
+++ 01_choixClient.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>templates</title>
+	<meta name="author" content="Jéros" />
+	<!-- Date: 2019-10-31 -->
+</head>
+<body>
+	<form name="frm" method="post" action="10_resultat_formulaire_simple.php">
+		<!-- champs simple -->
+		Acteur : <input type="text" name="zl_acteur" ><br /><br />
+		Thème : <input type="text" name="zl_theme" ><br /><br />
+		<input type="submit" value="Valider" />
+	</form>
+</body>
+</html>
